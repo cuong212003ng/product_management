79905f0042eea94a8ef330fcc6abc1ec0603dd57
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-11-15 17:17:16 +0700

    Trang danh sach san pham phia client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,21 @@
-module.exports.products =   (req, res) => {
+const Product = require('../../model/product.model')
+
+module.exports.products = async (req, res) => {
+
+
+    const products = await Product.find({
+        status: 'active',
+        deleted: false,
+    })
+
+    products.forEach(item => {
+        item.oldPrice = item.price * (1 + item.discountPercentage / 100)
+    })
+
+    console.log(products)
+
     res.render('client/pages/products/index',{
-        titlePage: 'Danh sách sản phẩm'
+        titlePage: 'Danh sách sản phẩm',
+        products: products
     })
 }
