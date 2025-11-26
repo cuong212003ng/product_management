3dc6632e565f128a3a80ad63ae0a234d9f6d9f1a
Cuong <cuong10d4hungvuongk13@gmail.com>
2025-11-26 14:30:22 +0700

    Chuc nang tim kiem san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -29,6 +29,8 @@ module.exports.products = async (req, res) => {
         filterStatus[index].class = 'active'
     }
 
+    //End Filter status
+
     let find = {
         deleted: false
     }
@@ -37,11 +39,29 @@ module.exports.products = async (req, res) => {
         find.status = req.query.status
     }
     
+    //Search
+
+    let keyword = ""
+
+    if(req.query.keyword) {
+        keyword = req.query.keyword
+
+        const regex = new RegExp(keyword, 'i') // tim hieu them kien thuc regex
+
+        find.title = regex
+    }
+
+    //End Search
+
+
+
+
     const products = await Product.find(find)
 
     res.render('admin/pages/products/index', {
         titlePage: 'Quản lý',
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     })
 }
