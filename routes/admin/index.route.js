a909ee4218ffc711ab13ecbeed0e3cec4353f51f
Cuong <cuong10d4hungvuongk13@gmail.com>
2026-01-12 08:34:27 +0700

    fix code

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -13,11 +13,11 @@ module.exports = (app) => {
 
     const PATH_ADMIN = systemConfig.prefixAdmin
 
-    app.use(PATH_ADMIN +'/dashboard', middlewareAuth.requireLogin, dashboardRoute)
-    app.use(PATH_ADMIN +'/products', middlewareAuth.requireLogin, productsRoute)
-    app.use(PATH_ADMIN +'/categories/products', middlewareAuth.requireLogin, categoriesProductsRoute)
+    app.use(PATH_ADMIN +'/dashboard',middlewareAuth.requireLogin, dashboardRoute)
+    app.use(PATH_ADMIN +'/products',middlewareAuth.requireLogin, productsRoute)
+    app.use(PATH_ADMIN +'/categories/products',middlewareAuth.requireLogin, categoriesProductsRoute)
     app.use(PATH_ADMIN +'/roles', middlewareAuth.requireLogin, rolesRoute)
-    app.use(PATH_ADMIN +'/accounts', middlewareAuth.requireLogin, accountsRoute)
+    app.use(PATH_ADMIN +'/accounts',middlewareAuth.requireLogin, accountsRoute)
     app.use(PATH_ADMIN +'/auth', authRoute)
 
 }
