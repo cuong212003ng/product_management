1cae318bcc308ca1cb5affaad3438f61e960b782
Cuong <cuong10d4hungvuongk13@gmail.com>
2026-01-13 11:17:38 +0700

    Chuc nang cap quyen(chua xong)

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,20 +1,26 @@
 const systemConfig = require('../../config/system')
 const Accounts = require('../../model/accounts.model')
+const Roles = require('../../model/roles.model')
 
 module.exports.requireLogin = async (req, res, next) => {
-    
-    if(!req.cookies.token){
+
+    if (!req.cookies.token) {
         return res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
     } else {
 
         const user = await Accounts.findOne({
             token: req.cookies.token,
             deleted: false
-        })
-        if(!user){
+        }).select('-password -token -deleted -deletedAt -createdAt -updatedAt')
+        if (!user) {
             return res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
         } else {
-            req.user = user
+            const role = await Roles.findOne({
+                _id: user.role_id
+            }).select('title permissions')
+
+            res.locals.user = user // biến toàn cục
+            res.locals.role = role
             next()
         }
     }
